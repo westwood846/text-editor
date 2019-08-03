4ecf6427a0f549a9e79af66121d086169fb5cc3e
turbopope <PapstDonB@Googlemail.com>
2019-08-03 16:45:52 +0800

    Style and position the text input

--- src/components/input.js
+++ src/components/input.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './input.css';
 
 class Input extends React.Component {
   constructor(props) {
@@ -19,8 +20,13 @@ class Input extends React.Component {
   }
 
   render() {
+    let style = {
+      "fontFamily": "Roboto",
+      "fontSize": "24pt"
+    }
+
     return (
-      <input type="text" onChange={this.handleInputChange} value={this.state.input}></input>
+      <input type="text" onChange={this.handleInputChange} value={this.state.input} style={style} contentEditable="true"></input>
     )
   }
 }
